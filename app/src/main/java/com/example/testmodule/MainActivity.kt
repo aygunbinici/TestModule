d7fe1f0bffd42ebeab451b754c46f8ab26cc983c
aygunbinici <aygunbinici35@gmail.com>
2024-05-27 01:12:21 +0300

    init commit

---
+++ app/src/main/java/com/example/testmodule/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.testmodule
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mathlibrary.MathLibrary
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val mathLibrary =MathLibrary()
+
+        val topla=mathLibrary.topla(1,1)
+        val cikar=mathLibrary.cikar(10,5)
+        val carp=mathLibrary.carp(10,10)
+        Log.d("MATH TOPLAMA",topla.toString())
+        Log.d("MATH CIKARMA",cikar.toString())
+        Log.d("MATH CARPMA",carp.toString())
+    }
+}
---
+++ mathLibrary/src/main/java/com/example/mathlibrary/MathLibrary.kt
@@ -0,0 +1,14 @@
+package com.example.mathlibrary
+
+class MathLibrary {
+
+    fun topla(value1:Int, value2:Int):Int {
+        return value1+value2
+    }
+    fun cikar(value1:Int, value2:Int):Int {
+        return value1-value2
+    }
+    fun carp(value1:Int, value2:Int):Int {
+        return value1*value2
+    }
+}
