655af0c98f040a4bcf32be741eba808311718e6d
aygun.binici@univera.com.tr <aygun.binici@univera.com.tr>
2024-10-04 15:01:33 +0300

    pi add
    show pi added

--- mathLibrary/src/main/java/com/example/mathlibrary/MathLibrary.kt
+++ mathLibrary/src/main/java/com/example/mathlibrary/MathLibrary.kt
@@ -1,5 +1,8 @@
 package com.example.mathlibrary
 
+import android.content.Context
+import android.widget.Toast
+
 class MathLibrary {
 
     fun topla(value1:Int, value2:Int):Int {
@@ -10,5 +13,9 @@ class MathLibrary {
     }
     fun carp(value1:Int, value2:Int):Int {
         return value1*value2
+    }
+    fun showPI(context: Context){
+        val pi = Math.PI.toString()
+        Toast.makeText(context,pi,Toast.LENGTH_LONG).show()
     }
 }
